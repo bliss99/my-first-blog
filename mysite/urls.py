dec9f4ef87b75669018519a3ab4da14daf045103
kspyo <bliss99@naver.com>
2016-03-25 09:15:57 +0900

    add user, login, logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/', auth_views.login, name='login',kwargs={'template_name': 'login.html'}),
+    url(r'^accounts/logout/', auth_views.logout, name='logout',kwargs={'next_page': '/'}),
+    url(r'^register/', CreateView.as_view(
+            template_name='register.html',
+            form_class=UserCreationForm,
+            success_url='/'), name='signup'
+    ),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'', include('blog.urls')),
 ]
